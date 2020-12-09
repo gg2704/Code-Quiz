e50298d429a95f7e743182f5da84673613afdead
gg2704 <72413403+gg2704@users.noreply.github.com>
2020-12-08 20:12:15 -0700

    Add files via upload

---
+++ Homework4questions.js
@@ -0,0 +1,28 @@
+
+    const questions = [
+        {
+            title: "Which of the following tag is used for inserting the largest heading in HTML?",
+            choices: ["h3", "h1", "h5", "h6"],
+            answer: "h1"
+        },
+        {
+            title: "Which is used to represent the closing of a tag in HTML?",
+            choices: ["+", "@", "!", "/"],
+            answer: "/"
+        },
+        {
+            title: "What is the HTML tag under which one can write the JavaScript code?",
+            choices: ["javascript", "undefined", "script", "js"],
+            answer: "script"
+        },
+        {
+            title: " Which of the following is a Javascript data type?",
+            choices: ["null", "undefined", "object", "all of the above"],
+            answer: "all of the above"
+        },
+        {
+            title: "Which of the following function of String object combines the text of two strings and returns a new string?",
+            choices: ["add", "concat", "merge", "append"],
+            answer: "concat"
+        }
+    ];
